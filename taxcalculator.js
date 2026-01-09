0a21687d22de6f10512c7a0235071751c3a938fe
Akshata B Managanvi <135851838+Akshatamanaganvi@users.noreply.github.com>
2026-01-09 19:53:11 +0530

    Update taxcalculator.js

--- taxcalculator.js
+++ taxcalculator.js
@@ -1,1 +1,13 @@
+function calculate() {
+  const income = document.getElementById("income").value;
+  let tax = 0;
+
+  if (income <= 250000) tax = 0;
+  else if (income <= 500000) tax = income * 0.05;
+  else if (income <= 1000000) tax = income * 0.2;
+  else tax = income * 0.3;
+
+  document.getElementById("result").innerText =
+    "Calculated Tax: ₹" + tax;
+}
 
